11411daf681da4d9245623c0e7f9c6dd6ef9017f
Thibault Duperron <zomzog@gmail.com>
2026-05-23 10:39:49 +0200

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -77,7 +77,7 @@ kover {
         verify {
             rule {
                 bound {
-                    minValue = 100
+                    minValue = 95
                     coverageUnits = CoverageUnit.LINE
                     aggregationForGroup = AggregationType.COVERED_PERCENTAGE
                 }
